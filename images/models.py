0025041456039c88482882fa8cc5d40f1de28b20
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-07-03 13:09:18 +0300

    set up the media configurations

--- images/models.py
+++ images/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+import datetime as dt
+
+class gallery(models.Model):
+    title = models.CharField(max_length=55)
+    category = models.CharField(max_length=30)
+    image_path = models.ImageField(upload_to = 'gallery/')
+
