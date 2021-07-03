8dda4496d80ac48b8b0a42c81b97511fc0b871f2
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-07-03 15:09:04 +0300

    add pub date and

--- images/models.py
+++ images/models.py
@@ -7,7 +7,23 @@ class gallery(models.Model):
     title = models.CharField(max_length=55)
     description = models.TextField(max_length=100)
     image_path = models.ImageField(upload_to = 'gallery/')
+    pub_date = models.DateTimeField(auto_now_add=True)
     # category =  models.ForeignKey('Editor',on_delete=models.CASCADE)
     # location = models.
-    
+
+
+    def __str__(self):
+        return self.first_name
+    class Meta:
+        ordering = ['first_name']
+
+    def save_image(self):
+        self.save()
+
+class location(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
 
